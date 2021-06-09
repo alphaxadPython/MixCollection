b939e5935264301a0bf4791b242f69420685a04b
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-09 11:42:01 +0300

    modified server for users

--- userServer.php
+++ userServer.php
@@ -2,9 +2,25 @@
 
     include "connection.php";
 
-    if isset(){
+    if isset($_POST['signup']){
         $username = mysqli_real_escape_string($conn, $_POST['username']);
-        $email = mysqli_real_escape_string($conn, $_POST['username']);
-        $password = mysqli_real_escape_string($conn, $_POST['username']);
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+        $password1 = mysqli_real_escape_string($conn, $_POST['password1']);
+        $password2 = mysqli_real_escape_string($conn, $_POST['password2']);
+
+
+        if($password1 == $password2){
+
+            $sql = "INSERT INTO users values()";
+            mysqli_query($conn, $sql);
+            
+        }
     }
+
+
+
+
+
+
+
 ?>
