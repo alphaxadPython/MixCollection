71d2d31707144d923ed77bb3824ea28948f73d36
alphaxadPython <alphaxadcoder@gmail.com>
2021-06-09 11:24:41 +0300

    changes on navigation menues

--- cartegory.php
+++ cartegory.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cartegories | MixCollection</title>
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/main.css">
@@ -16,40 +16,8 @@
 </head>
 <body>
     
-    <nav class="navbar navbar-expand-sm navbar-light bg-light">
-        <a class="navbar-brand" href="#">Mix Collection</a>
-        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="collapsibleNavId">
-               
-        <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="text" placeholder="Search...">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
-            </form>
-            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-              
-                <li class="nav-item active">
-                    <a class="nav-link" href="" > <i class="fa fa-home" aria-hidden="true"></i> Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=""> <i class="fa fa-user" aria-hidden="true"></i> Profile</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=""> <i class="fa fa-cart-plus" aria-hidden="true"></i> Cart</a>
-                </li><li class="nav-item">
-                    <a class="nav-link" href=""> <i class="fa fa-star" aria-hidden="true"></i> Checkout</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href=""><i class="fa fa-unlock" aria-hidden="true"></i> Logout</a>
-                </li>
-             
-            </ul>
-        </div>
-    </nav>
+    <?php include "nav1.php" ?>
 
-   
 	<div class="section">
         <div id="slider-carousel" class="carousel slide" data-ride="carousel">
             <ol class="carousel-indicators">
---
+++ nav1.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-sm navbar-light bg-light">
+        <a class="navbar-brand" href="#">Mix Collection</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+               
+        <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2" type="text" placeholder="Search...">
+                <button class="btn btn-outline-success my-2 my-sm-0" type="submit"><i class="fa fa-search" aria-hidden="true"></i></button>
+            </form>
+            <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+              
+                <li class="nav-item active">
+                    <a class="nav-link" href="" > <i class="fa fa-home" aria-hidden="true"></i> Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href=""> <i class="fa fa-user" aria-hidden="true"></i> Profile</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href=""> <i class="fa fa-cart-plus" aria-hidden="true"></i> Cart</a>
+                </li><li class="nav-item">
+                    <a class="nav-link" href=""> <i class="fa fa-star" aria-hidden="true"></i> Checkout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href=""><i class="fa fa-unlock" aria-hidden="true"></i> Logout</a>
+                </li>
+             
+            </ul>
+        </div>
+    </nav>
+
+   
